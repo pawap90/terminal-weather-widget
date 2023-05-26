329a69bf7c8ab65b5d2bfe9958d70152375a0a5e
Paula Santamaría <m.paula.santamaria@gmail.com>
2023-05-26 19:16:05 -0300

    add update interval

--- src/index.ts
+++ src/index.ts
@@ -8,26 +8,37 @@ const program = new Command();
 const options = {
     lat: -34.6037,
     lon: -58.3816,
-    timezone: 'America/Argentina/Buenos_Aires'
+    timezone: 'America/Argentina/Buenos_Aires',
+    updateInterval: 5
 };
 
 program
+    .name('weather')
     .argument('[lat]', 'latitude', options.lat)
     .argument('[lon]', 'longitude', options.lon)
     .argument('[timezone]', 'timezone', options.timezone)
-    .action((lat, lon, timezone) => {
+    .argument('[updateInterval]', 'updateInterval', options.updateInterval)
+    .action((lat, lon, timezone, updateInterval) => {
         if (lat) options.lat = lat as number;
         if (lon) options.lon = lon as number;
         if (timezone) options.timezone = timezone as string;
+        if (updateInterval) options.updateInterval = updateInterval as number;
     });
 
 program.parse(process.argv);
 
-(async () => {
-    const weatherInfo = await getCurrentWeather(
-        options.lat,
-        options.lon,
-        options.timezone
-    );
-    console.log(format(weatherInfo));
-})();
+printUpdatedWeather();
+
+if (options.updateInterval < 1)
+    throw new Error('[updateInterval] must be at least 1 minute');
+
+setInterval(printUpdatedWeather, options.updateInterval * 60 * 1000);
+
+function printUpdatedWeather() {
+    getCurrentWeather(options.lat, options.lon, options.timezone)
+        .then((weatherInfo) => {
+            console.clear();
+            console.log(format(weatherInfo));
+        })
+        .catch((err) => console.error(err));
+}
