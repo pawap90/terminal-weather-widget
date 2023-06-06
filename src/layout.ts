2ff22fb5c874d9e05840cad3a2e0b398147a3f52
Paula Santamaría <m_paula_santamaria@live.com>
2023-06-05 21:13:29 -0300

    add formatting for forecast info

--- src/layout.ts
+++ src/layout.ts
@@ -1,40 +1,68 @@
 import { TableUserConfig, getBorderCharacters, table } from 'table';
-import type { WeatherInfo } from './weather';
-import { palette } from "./color";
+import type { ForecastInfo, WeatherInfo } from './weather';
+import { palette } from './color';
+
+export function format(forecast: ForecastInfo[]): string;
+export function format(weatherInfo: WeatherInfo): string;
+export function format(data: WeatherInfo | ForecastInfo[]): string {
+    if (isWeatherInfo(data)) {
+        const tableData = [
+            [formatDate(data.time), ''],
+            [data.icon, formatWeatherData(data)]
+        ];
+
+        const config: TableUserConfig = {
+            columns: [
+                { alignment: 'left', width: 20 },
+                {
+                    alignment: 'left',
+                    paddingLeft: 4,
+                    paddingRight: 4,
+                    width: 16,
+                    verticalAlignment: 'middle',
+                    wrapWord: true
+                }
+            ],
+            spanningCells: [
+                { col: 0, row: 0, colSpan: 2, alignment: 'center' },
+                {
+                    col: 0,
+                    row: 1,
+                    rowSpan: 1,
+                    verticalAlignment: 'middle',
+                    paddingLeft: 3
+                }
+            ],
+            border: getBorderCharacters('norc')
+        };
+
+        return table(tableData, config);
+    }
+    else {
+        const tableData = [];
+
+        for (const forecast of data) {
+            tableData.push([
+                `${formatDate(forecast.time)}\n${forecast.description}`,
+                 forecast.icon,
+                `${palette.yellow.inverse(`${forecast.temperatureMax}°C`)}\n${forecast.temperatureMin}°C`,
+            ]);
+        }
+    
+        return table(tableData, {
+            border: getBorderCharacters('norc'),
+            header: { content: palette.pink('Forecast') },
+            columnDefault: { verticalAlignment: 'middle', paddingLeft: 3, paddingRight: 3 },
+            drawHorizontalLine: () => true,
+            drawVerticalLine: () => false,
+
+        });
+    }
+}
 
 
-export function format(weatherInfo: WeatherInfo): string {
-    const tableData = [
-        [formatDate(weatherInfo.time), ''],
-        [weatherInfo.icon, formatWeatherData(weatherInfo)]
-    ];
-
-    const config: TableUserConfig = {
-        columns: [
-            { alignment: 'left', width: 20 },
-            {
-                alignment: 'left',
-                paddingLeft: 4,
-                paddingRight: 4,
-                width: 16,
-                verticalAlignment: 'middle',
-                wrapWord: true
-            }
-        ],
-        spanningCells: [
-            { col: 0, row: 0, colSpan: 2, alignment: 'center' },
-            {
-                col: 0,
-                row: 1,
-                rowSpan: 1,
-                verticalAlignment: 'middle',
-                paddingLeft: 3
-            }
-        ],
-        border: getBorderCharacters('norc')
-    };
-
-    return table(tableData, config);
+function isWeatherInfo(data: WeatherInfo | ForecastInfo[]): data is WeatherInfo {
+    return data.hasOwnProperty('temperature');
 }
 
 function formatWeatherData(weatherInfo: WeatherInfo) {
@@ -51,10 +79,12 @@ function formatWeatherData(weatherInfo: WeatherInfo) {
  * E.g: Wed, May 24, 2023, 08:20 PM
  */
 function formatDate(date: Date): string {
-    return palette.gray(date.toLocaleString('en-US', {
-        weekday: 'short',
-        day: '2-digit',
-        month: 'short',
-        year: 'numeric'
-    }));
+    return palette.gray(
+        date.toLocaleString('en-US', {
+            weekday: 'long',
+            day: '2-digit',
+            month: 'short',
+            year: 'numeric'
+        })
+    );
 }
